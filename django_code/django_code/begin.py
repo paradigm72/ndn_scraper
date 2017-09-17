5c8b828821d3670048f462828ceff8c19400daa0
Paul Romine <promine@gmail.com>
2017-09-16 21:47:31 -0500

    try to add cross-domain ok even when http request fails

--- django_code/django_code/begin.py
+++ django_code/django_code/begin.py
@@ -5,9 +5,9 @@ import crossdomain_response
 
 def getPosts(request):
     # add cross-site headers to request - not currently implemented
-    #request = middleware.XsSharing.process_request(request=request)
+    response_string = crossdomain_response.process_request(request=request)
 
-    response_string=html_core.getAllPosts(html_core.getFullHTML("http://www.ndnation.com/boards/index.php?football"))
+    response_string = response_string & html_core.getAllPosts(html_core.getFullHTML("http://www.ndnation.com/boards/index.php?football"))
     myResponseObj = HttpResponse(response_string,mimetype='text/plain',status=200)
 
     # add cross-site headers to response
